16c555377150f4d353d8e19f1323868f7a86d7cc
Philippe <philippe.branly@gmail.com>
2021-12-12 15:14:30 +0100

    Update pygazpar_to_influxdb.py

--- pygazpar_to_influxdb.py
+++ pygazpar_to_influxdb.py
@@ -14,7 +14,7 @@ from decimal import Decimal
 import pygazpar
 import os
 import paho.mqtt.client as mqtt
-import urlparse
+import urllib.parse as urlparse
 
 url_influxdb = os.environ['PYGAZPAR_INFLUXDB2_HOST']
 bucket_influxdb = os.environ['PYGAZPAR_INFLUXDB2_BUCKET']
