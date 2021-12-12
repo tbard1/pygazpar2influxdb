43f534aa8834604d5f0ace169bd59caa06a3320c
Philippe <philippe.branly@gmail.com>
2021-12-12 16:52:33 +0100

    Update pygazpar_to_influxdb.py

--- pygazpar_to_influxdb.py
+++ pygazpar_to_influxdb.py
@@ -26,10 +26,19 @@ password_pygazpar = os.environ['PYGAZPAR_PYGAZPAR_PASSWORD']
 pce_pygazpar = os.environ['PYGAZPAR_PCE_IDENTIFIER']
 pce_lastNDays = int(os.environ['PYGAZPAR_LASTNDAY'])
 
-url_mqtt = os.environ['PYGAZPAR_MQTT_URL']
-url_username = os.environ['PYGAZPAR_MQTT_LOGIN']
-url_password = os.environ['PYGAZPAR_MQTT_PASSWORD']
+broker = os.environ['PYGAZPAR_MQTT_URL']
+port = 1883
+username = os.environ['PYGAZPAR_MQTT_LOGIN']
+password = os.environ['PYGAZPAR_MQTT_PASSWORD']
 #xxxxxxxxxxxx = os.environ['PYGAZPAR_XXXXXXXXXXXXXXX']
+topic = "/python/mqtt"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+
+topic = "/python/mqtt"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
 
 parser = argparse.ArgumentParser()
 
@@ -88,82 +97,40 @@ write_api.write(bucket=bucket_influxdb, record=jsonInflux)
 
 #############################################################################################################################
 
-# Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# 3. Neither the name of mosquitto nor the names of its
-#   contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
 
 
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
 
 
-# Define event callbacks
-def on_connect(client, userdata, flags, rc):
-    print("rc: " + str(rc))
-
-def on_message(client, obj, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-def on_publish(client, obj, mid):
-    print("mid: " + str(mid))
-
-def on_subscribe(client, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-def on_log(client, obj, level, string):
-    print(string)
-
-mqttc = mqtt.Client()
-# Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
-
-# Uncomment to enable debug messages
-mqttc.on_log = on_log
-
-# Parse CLOUDMQTT_URL (or fallback to localhost)
-url_str = os.environ.get('url_mqtt:1883')
-url = urlparse.urlparse(url_str)
-#topic = url.path[1:] or 'test'
-
-# Connect
-mqttc.username_pw_set(url_username, url_password)
-#mqttc.connect(url.hostname, url.port)
-mqttc.connect(url, 1883)
-
-# Start subscribe, with QoS level 0
-mqttc.subscribe(topic, 0)
-
-# Publish a message
-mqttc.publish(topic, "my message")
-
-# Continue the network loop, exit when an error occurs
-rc = 0
-while rc == 0:
-    rc = mqttc.loop()
-print("rc: " + str(rc))
+if __name__ == '__main__':
+    run()
