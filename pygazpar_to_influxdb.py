baab06e041c4991d74c8a0b7b490f8630d6d67cf
Thomas <tom@bard1.fr>
2022-01-01 19:01:32 +0100

    Add a schedule

--- pygazpar_to_influxdb.py
+++ pygazpar_to_influxdb.py
@@ -13,8 +13,11 @@ import decimal
 from decimal import Decimal
 import pygazpar
 import os
+import schedule
+import time
+from dotenv import load_dotenv
 
-
+load_dotenv()
 url_influxdb = os.environ['PYGAZPAR_INFLUXDB2_HOST']
 bucket_influxdb = os.environ['PYGAZPAR_INFLUXDB2_BUCKET']
 token_influxdb = os.environ['PYGAZPAR_INFLUXDB2_TOKEN']
@@ -34,7 +37,7 @@ parser.add_argument("-v", "--verbose", dest="verbose_count", action="count", def
 
 args = parser.parse_args()
 log = logging.getLogger()
-logging.basicConfig(stream=sys.stderr, level=logging.WARNING,
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG,
                     format='%(name)s (%(levelname)s): %(message)s')
 log.setLevel(max(3 - args.verbose_count, 0) * 10)
 
@@ -45,37 +48,47 @@ influxclient = InfluxDBClient(url=url_influxdb, token=token_influxdb, org=org_in
 write_api = influxclient.write_api(write_options=SYNCHRONOUS)
 
 #------------------------------------------------- 
-        
-client = pygazpar.Client(username=login_pygazpar, password=password_pygazpar, pceIdentifier=pce_pygazpar, meterReadingFrequency=Frequency.DAILY, lastNDays=pce_lastNDays, tmpDirectory='/tmp')
+ 
+def g2i():
+  log.debug('Entering Function')
 
-log.debug('Starting to update pygazpar data')
-client.update()
-log.debug('End update pygazpar data')
+  client = pygazpar.Client(username=login_pygazpar, password=password_pygazpar, pceIdentifier=pce_pygazpar, meterReadingFrequency=Frequency.DAILY, lastNDays=pce_lastNDays, tmpDirectory='/tmp')
 
-data = client.data()
+  log.debug('Starting to update pygazpar data')
+  client.update()
+  log.debug('End update pygazpar data')
 
-jsonInflux = []
+  data = client.data()
 
-for measure in data:
-    print(measure)
-    date_time_obj = datetime.datetime.strptime(measure['time_period'], '%d/%m/%Y')
-    if 'start_index_m3' in measure:
-      jsonInflux.append({
-        "measurement": "gazpar_consumption_per_day",
-        "tags": {
-        },
-        "time": date_time_obj.strftime('%Y-%m-%dT%H:%M:%S'),
-        "fields": {
-            "value": measure['volume_m3'],
-            "start_index_m3": measure['start_index_m3'],
-            "end_index_m3": measure['end_index_m3'],
-            "volume_m3": measure['volume_m3'],
-            "energy_kwh": measure['energy_kwh'],
-            "converter_factor_kwh/m3": measure['converter_factor_kwh/m3'],
-            "type": measure['type'],
-        }
-    })
-    else:
-      print('No measure')
+  jsonInflux = []
 
-write_api.write(bucket=bucket_influxdb, record=jsonInflux)
+  for measure in data:
+      print(measure)
+      date_time_obj = datetime.datetime.strptime(measure['time_period'], '%d/%m/%Y')
+      if 'start_index_m3' in measure:
+        jsonInflux.append({
+          "measurement": "gazpar_consumption_per_day",
+          "tags": {
+          },
+          "time": date_time_obj.strftime('%Y-%m-%dT%H:%M:%S'),
+          "fields": {
+              "value": measure['volume_m3'],
+              "start_index_m3": measure['start_index_m3'],
+              "end_index_m3": measure['end_index_m3'],
+              "volume_m3": measure['volume_m3'],
+              "energy_kwh": measure['energy_kwh'],
+              "converter_factor_kwh/m3": measure['converter_factor_kwh/m3'],
+              "type": measure['type'],
+          }
+      })
+      else:
+        print('No measure')
+
+  write_api.write(bucket=bucket_influxdb, record=jsonInflux)
+
+schedule.every().day.at("01:00").do(g2i)
+
+while True:
+   schedule.run_pending()
+time.sleep(1)
+
